5e9fcdfad223042147f18dc67770f000ebcbc991
Rizwan Sattar <rsattar@gmail.com>
2015-11-07 22:26:44 -0800

    Add barebones iOS and tvOS apps

---
+++ Hauth tvOS App/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Hauth tvOS App
+//
+//  Created by Rizwan Sattar on 11/7/15.
+//  Copyright © 2015 Rizwan Sattar. All rights reserved.
+//
+
+import UIKit
+import Hauth
+
+class ViewController: UIViewController {
+
+    var client: HauthClient!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
