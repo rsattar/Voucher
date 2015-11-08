eb8cd176622a382851cd312541ed45bb7601b78b
Rizwan Sattar <rsattar@gmail.com>
2015-11-07 22:26:10 -0800

    Include HauthStreamsController as public
    This is because both server and client import streams controller in their (public) .h's. Le sigh.

--- Hauth/Hauth.h
+++ Hauth/Hauth.h
@@ -15,6 +15,7 @@ FOUNDATION_EXPORT double HauthVersionNumber;
 FOUNDATION_EXPORT const unsigned char HauthVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Hauth/PublicHeader.h>
+#import <Hauth/HauthStreamsController.h>
 #import <Hauth/HauthClient.h>
 #import <Hauth/HauthServer.h>
 
