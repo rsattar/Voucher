e286cb6ee735c4c734f3c3279e88f28b345ec8dd
Rizwan Sattar <rsattar@gmail.com>
2015-11-09 20:02:59 -0800

    Apparently in renaming the project, fixed imports

--- Voucher/Voucher.h
+++ Voucher/Voucher.h
@@ -16,13 +16,6 @@ FOUNDATION_EXPORT const unsigned char VoucherVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Voucher/PublicHeader.h>
 
-// NOTE: In Xcode 7.1, if two frameworks have the same module name ("Voucher"),
-// then sometimes one of the frameworks (in my case, tvOS) doesn't compile correctly,
-// complaining of the inclusion of "non-modular headers". So make these
-// non modular import calls here.
-//#import "VoucherStreamsController.h"
-//#import "VoucherClient.h"
-//#import "VoucherServer.h"
 #import <Voucher/VoucherCommon.h>
 #import <Voucher/VoucherStreamsController.h>
 #import <Voucher/VoucherClient.h>
