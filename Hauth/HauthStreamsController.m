bb75d7ff42b40bdbe422ef6c03fdbc7c8afaea35
Rizwan Sattar <rsattar@gmail.com>
2015-11-09 18:20:05 -0800

    Fix sending data; Add length preamble to data sent
    Fixed sending of data, and also fixed receiving of data. Ultimately I have to prepend the 'content length' preamble to the data being sent, and checking for that on the other end, so we know at what point to stop listening for more data and parse the received NSData as a dictionary

--- Hauth/HauthStreamsController.m
+++ Hauth/HauthStreamsController.m
@@ -6,13 +6,14 @@
 //  Copyright © 2015 Rizwan Sattar. All rights reserved.
 //
 
-#import "HauthStreamsController.h"
+#import <Hauth/HauthStreamsController.h>
 
 static NSUInteger const BUFFER_SIZE = 1024;
 
 @interface HauthStreamsController () <NSStreamDelegate>
 
 @property (strong, nonatomic) NSMutableData *dataReadBuffer;
+@property (assign, nonatomic) int64_t expectedReadLength;
 @property (strong, nonatomic) NSMutableData *dataSendBuffer;
 @property (assign, nonatomic) NSUInteger sendBufferByteIndex;
 
@@ -35,8 +36,16 @@ static NSUInteger const BUFFER_SIZE = 1024;
 {
     // TODO(Riz): Queue up multiple sendData: requests,
     // so they don't stomp on each other
-    self.dataSendBuffer = [NSMutableData dataWithData:data];
+    self.dataSendBuffer = [NSMutableData dataWithCapacity:8+data.length];
+    // First, append the length of the data, as a long
+    uint64_t lengthPreamble = htonll((uint64_t)data.length);
+    [self.dataSendBuffer appendBytes:&lengthPreamble length:sizeof(lengthPreamble)];
+    // Then append the actual data
+    [self.dataSendBuffer appendData:data];
     self.sendBufferByteIndex = 0;
+    if (self.outputStream.hasSpaceAvailable) {
+        [self flushSendDataIfAvailable];
+    }
 }
 
 
@@ -112,54 +121,53 @@ static NSUInteger const BUFFER_SIZE = 1024;
         case NSStreamEventHasSpaceAvailable: {
             assert(stream == self.outputStream);
             NSLog(@"Output stream has space available");
-            // Send any data in our write buffer
-            uint8_t *readBytes = (uint8_t *) [self.dataSendBuffer mutableBytes];
-            // Move along the byte array up to the sent index
-            readBytes += self.sendBufferByteIndex;
-            NSUInteger bytesToWrite = MIN(BUFFER_SIZE, self.dataSendBuffer.length - self.sendBufferByteIndex);
-
-            uint8_t buffer[BUFFER_SIZE];
-            // Copy the data we want to send over to this buffer
-            (void)memcpy(buffer, readBytes, bytesToWrite);
-
-            NSInteger bytesWritten = [self.outputStream write:buffer maxLength:bytesToWrite];
-            if (bytesWritten < 0) {
-                // Some error occurred, yikes. Handle write errors in NSStreamEventErrorOccurred
-                NSLog(@"Couldn't write to output stream");
-            } else {
-                NSLog(@"Sent %ld bytes", (long) bytesWritten);
-                self.sendBufferByteIndex += bytesWritten;
-
-                if (self.sendBufferByteIndex == self.dataSendBuffer.length) {
-                    NSLog(@"Finished sending whole data buffer");
-                    self.dataSendBuffer = nil;
-                    self.sendBufferByteIndex = 0;
-                }
-            }
+            [self flushSendDataIfAvailable];
         } break;
 
         case NSStreamEventHasBytesAvailable: {
             if (!self.dataReadBuffer) {
                 self.dataReadBuffer = [NSMutableData data];
+                self.expectedReadLength = -1;
             }
 
             assert(stream == self.inputStream);
 
-            uint8_t buffer[BUFFER_SIZE];
+            while (self.inputStream.hasBytesAvailable) {
+                if (self.expectedReadLength < 0) {
+                    // We haven't read anything yet, so just read the length preamble
+                    NSUInteger numPreambleBytes = sizeof(uint64_t);
+                    uint8_t buffer[numPreambleBytes];
+                    NSInteger bytesRead = [self.inputStream read:buffer maxLength:numPreambleBytes];
+                    if (bytesRead == numPreambleBytes) {
+                        uint64_t contentLength;
+                        memcpy(&contentLength, buffer, numPreambleBytes);
+                        NTOHLL(contentLength);
+                        self.expectedReadLength = contentLength;
+                    }
+                } else {
+                    // We already know our expected size, so start or continue reading
 
-            NSInteger bytesRead = [self.inputStream read:buffer maxLength:BUFFER_SIZE];
-            if (bytesRead < 0) {
-                NSLog(@"bytesRead: %ld", (long)bytesRead);
-                // Handle read errors in NSStreamEventErrorOccurred
-            } else if (bytesRead > 0) {
-                // We received some data.
-                NSLog(@"Received %ld bytes", (long)bytesRead);
-                [self.dataReadBuffer appendBytes:buffer length:bytesRead];
-            } else {
-                NSLog(@"Finished receiving bytes");
-                // End of buffer was reached. Do something about it?
-                [self handleReceivedData:[self.dataReadBuffer copy]];
-                self.dataReadBuffer = nil;
+                    uint8_t buffer[BUFFER_SIZE];
+
+                    NSInteger bytesRead = [self.inputStream read:buffer maxLength:BUFFER_SIZE];
+                    if (bytesRead < 0) {
+                        NSLog(@"bytesRead: %ld", (long)bytesRead);
+                        // Handle read errors in NSStreamEventErrorOccurred
+                    } else if (bytesRead > 0) {
+                        // We received some data.
+                        NSLog(@"Received %ld bytes", (long)bytesRead);
+                        [self.dataReadBuffer appendBytes:buffer length:bytesRead];
+                        if (self.dataReadBuffer.length >= self.expectedReadLength) {
+                            NSLog(@"Finished receiving bytes");
+                            // End of buffer was reached. Do something about it?
+                            [self handleReceivedData:[self.dataReadBuffer copy]];
+                            self.dataReadBuffer = nil;
+                            self.expectedReadLength = -1;
+                        }
+                    } else {
+                        NSLog(@"Read zero bytes from input stream. The end is nigh");
+                    }
+                }
             }
         } break;
 
@@ -168,14 +176,52 @@ static NSUInteger const BUFFER_SIZE = 1024;
             // fall through
         case NSStreamEventErrorOccurred:
             // fall through
+            NSLog(@"NSStreamErrorOccurred (fallthrough)");
         case NSStreamEventEndEncountered: {
             // Start server again?
             NSLog(@"NSStreamEventEndEncountered");
+            if (stream == self.inputStream) {
+                NSLog(@"   => Input Stream");
+            } else if (stream == self.outputStream) {
+                NSLog(@"   => Output Stream");
+            }
             [self handleStreamEnd:stream];
             //if (self.requestHandler) {
             //    [self startAdvertisingWithRequestHandler:self.requestHandler];
             // }
         } break;
+    }
+}
+
+
+- (void) flushSendDataIfAvailable
+{
+    if (self.dataSendBuffer.length <= 0) {
+        return;
+    }
+    // Send any data in our write buffer
+    uint8_t *readBytes = (uint8_t *) [self.dataSendBuffer mutableBytes];
+    // Move along the byte array up to the sent index
+    readBytes += self.sendBufferByteIndex;
+    NSUInteger bytesToWrite = MIN(BUFFER_SIZE, self.dataSendBuffer.length - self.sendBufferByteIndex);
+
+    uint8_t buffer[BUFFER_SIZE];
+    // Copy the data we want to send over to this buffer
+    (void)memcpy(buffer, readBytes, bytesToWrite);
+
+    NSInteger bytesWritten = [self.outputStream write:buffer maxLength:bytesToWrite];
+    if (bytesWritten < 0) {
+        // Some error occurred, yikes. Handle write errors in NSStreamEventErrorOccurred
+        NSLog(@"Couldn't write to output stream");
+    } else {
+        NSLog(@"Sent %ld bytes", (long) bytesWritten);
+        self.sendBufferByteIndex += bytesWritten;
+
+        if (self.sendBufferByteIndex == self.dataSendBuffer.length) {
+            NSLog(@"Finished sending whole data buffer");
+            self.dataSendBuffer = nil;
+            self.sendBufferByteIndex = 0;
+        }
     }
 }
 
